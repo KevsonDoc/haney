8d8f24fdf63c4e13bf7ffec291d9a78317a514bd
Kevson Filipe <kevsonfilipesantos@gmail.com>
2025-01-01 15:52:43 -0300

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,20 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Hanei API')
-    .setDescription('The cats API description')
+    .setDescription('Hanei API')
+    .addBearerAuth({
+      type: 'http', // Required: The type of the security scheme. For Bearer auth, this should be 'http'.
+      scheme: 'bearer', // Required: The name of the HTTP Authorization scheme to be used in the Authorization header.
+      bearerFormat: 'JWT', // Optional: A hint to the client to identify how the bearer token is formatted.
+      description: 'Enter JWT token', // Optional: A short description for security scheme.
+      name: 'Authorization', // Optional: The name of the header or query parameter to be used.
+      in: 'header', // Optional: The location of the API key. Valid values are 'query' or 'header'.
+    })
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.APPLICATION_PORT ?? 3333);
 }
 bootstrap();

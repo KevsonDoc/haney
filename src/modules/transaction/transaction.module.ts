f0f95ddef2d2de3c1fe4100623a7919270dbe838
Kevson Filipe <kevsonfilipesantos@gmail.com>
2025-01-18 16:37:11 -0300

    refactor: remove import of user module

--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from '../user/user.module';
 import { CategoryController } from './controllers/category.controller';
 import { PaymentCardController } from './controllers/payment-card.controller';
 import { TransactionController } from './controllers/transaction.controller';
@@ -9,7 +10,6 @@ import { PaymentCard, PaymentCardSchema } from './entities/payment-card.entity';
 import { CategoryService } from './service/category.service';
 import { PaymentCardService } from './service/payment-card.service.service';
 import { TransactionService } from './service/transaction.service';
-import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
